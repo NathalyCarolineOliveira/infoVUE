20e5a546f0e3fa5f19cc8c3cac10789d29b6ef90
GustavoCKruger <gustavo.kruger1601@gmail.com>
2024-03-01 16:31:06 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ import { ref } from 'vue';
     const idade = 16
     const contador = ref(0)
     const informações = 'Informações do usuário'
+    const email = 'gustavo.kruger1601@gmail.com'
+    const número = 4792595186
+    
 
     function inverter(texto) {
         return texto.split('').reverse().join('')
@@ -24,16 +27,29 @@ import { ref } from 'vue';
       }
     }
 
+    function multiplicar(){
+      const vezes = Number(prompt("Informe um número para multiplicar"))
+      contador.value = contador.value * vezes; 
+    }
+
+    function dividir(){
+      const dividir = Number(prompt("Informe um número para dividir"))
+      contador.value = contador.value / dividir;
+    }
+
     function limpar(){
       contador.value = 0;
     }
 </script>
 
 <template>
+  <div class="conteudo">
     <div class="info">
         <h1>{{ informações }}</h1>
         <p>Nome: {{ nome }} </p>
         <p>Idade: {{ idade }}  anos</p>
+        <p>Email: {{ email }}</p>
+        <p>Número de telefone: {{ número }}</p>menor que 10
         <hr />
         <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
         <p>O nome tem {{ nome.length }}  caracteres</p>
@@ -47,50 +63,58 @@ import { ref } from 'vue';
       <p>Valor: {{ contador }}</p>
       <button @click="incrementar()">incrementar</button>
       <button @click="decrementar()">decrementar</button>
+      <button @click="multiplicar()">vezes</button>
+      <button @click="dividir()">divisão</button>
       <button @click="limpar()">limpar</button>
-      <div :style="{backgroundColor: contador > 10 ? 'green' : 'red' }">
-        <p v-if="contador > 10">Valor maior que 10</p>
-        <p v-else>Valor menor que 10</p>
+      <div :style="{backgroundColor: contador % 2 === 0 ? 'green' : 'red'}">
+        <p v-if="contador % 2 === 0">Valor é par</p>
+        <p v-else>Valor é ímpar</p>
       </div>
     </div>
     </div>
-
+  </div>
 </template>
 
 <style scoped>
-.info, .contador {
-  background-color: crimson;
-  padding: 20px 30px;
-  color: rgb(183, 210, 219);
-  border-radius: 10px;
-}
+  .conteudo{
+    margin: 0px 0px 0px 300px;
+    min-width: 600px;
+  }
+  .info,
+  .contador {
+    background-color: mediumturquoise;
+    padding: 20px 30px;
+    color: black;
+    border-radius: 10px;
+  }
 
-h1, h2 {
-  color: white;
-  font-weight: bold;
-  font-size: 1.5em;
-}
+  h1 {
+    color: white;
+    font-weight: bold;
+    font-size: 1.5em;
+  }
 
-button {
+  button {
     margin: 10px;
     padding: 10px;
     border: 0;
     border-radius: 5px;
-    background-color: white;
+    background-color: lightblue;
     color: black;
     font-weight: bold;
     cursor: pointer;
   }
 
   button:hover {
-    background-color: gray;
+    background-color: cadetblue;
   }
 
   div {
     margin: 10px 0;
     padding: 10px;
     border-radius: 5px;
-    color: white;
+    color: black;
     font-weight: bold;
   }
+  
 </style>
